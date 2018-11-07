50c72040692f18a06e15de0873934cf95f8f00df
JONES.JACOB <2200361@fbisd.flourbluffschools.net>
2018-11-07 13:29:17 -0600

    unfinshed

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+
+def palindrome():
+   
+    for i in range(1000000):
+        num = i
+        num = str(i)
+        if num[2:] == num[5:1:-1]:
+            if num[2:]==num[5:1:-1]:
+               if num[2:]==num[5:1:-1]:
+                   print(num)
+          
+                           
+palindrome()
